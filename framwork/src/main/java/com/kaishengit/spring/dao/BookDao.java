049cdae88c2da6278e6c7f0cd078b02a9b98ee84
qiyawei <956342117@qq.com>
2016-04-01 17:09:13 +0800

    继续练习

--- framwork/src/main/java/com/kaishengit/spring/dao/BookDao.java
+++ framwork/src/main/java/com/kaishengit/spring/dao/BookDao.java
@@ -3,7 +3,7 @@ package com.kaishengit.spring.dao;
 /**
  * Created by qiyawei on 2016/3/30.
  */
-public class BookDao implements IuserDao {
+public class BookDao implements IUserDao {
     @Override
     public void save() {
         System.out.println("book save......");
